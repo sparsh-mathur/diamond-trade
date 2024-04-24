8e2aac14ccfe993c34e00f68570e4ec3ed4bca31
sparsh mathur <mathursparsh01@gmail.com>
2024-04-24 11:04:09 +0530

    bank details edit

--- app/controllers/bankDetails.controller.js
+++ app/controllers/bankDetails.controller.js
@@ -1,6 +1,6 @@
 const BankDetails = require("../models").bank_details;
 
-exports.addBankDetails = (req, res) => {
+exports.addBankDetails = async (req, res) => {
   const { userId } = req.params;
   const { account_number, branch_name, ifsc_code, account_holder_name } =
     req.body;
@@ -14,6 +14,28 @@ exports.addBankDetails = (req, res) => {
     res.status(400).send({ message: "All fields are required!" });
     return;
   }
+  const bank_details = await BankDetails.findOne({
+    where: { user_id: userId },
+  });
+  if (bank_details) {
+    bank_details.account_number = account_number;
+    bank_details.branch_name = branch_name;
+    bank_details.ifsc_code = ifsc_code;
+    bank_details.account_holder_name = account_holder_name;
+    bank_details
+      .save()
+      .then((bankDetails) => {
+        res.send({
+          message: "Bank details updated successfully",
+          data: bankDetails,
+        });
+      })
+      .catch((err) => {
+        res.status(500).send({ message: err.message });
+      });
+    return;
+  }
+
   BankDetails.create({
     account_number,
     branch_name,
